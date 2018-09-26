e42befde77e29f6dc93d8cc75531dc1aa6c3eac5
Florian R <florian.r.pro@gmail.com>
2018-09-26 10:00:27 +0200

    Update bdd.php

--- utils/bdd.php
+++ utils/bdd.php
@@ -2,7 +2,7 @@
 
 try{
   
-    $bdd=new PDO('mysql:host=localhost;dbname=planhelper;charset=utf8','admin','online@2017');
+    $bdd=new PDO('mysql:host=localhost;dbname=planhelper;charset=utf8','admin','');
 }
 catch(Exeption $e)
 {      
